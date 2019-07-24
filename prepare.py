8619f42adebd952e4345d3da3a4d58ea6e0ac30b
David Espinola <daver787@gmail.com>
2019-07-24 15:30:50 -0500

    More work in the curriculum.

--- prepare.py
+++ prepare.py
@@ -1,36 +1,24 @@
-import pandas as pd
-import numpy as np
-
-import matplotlib.pyplot as plt
-%matplotlib inline
-import seaborn as sns
-
-# ignore warnings
-import warnings
-warnings.filterwarnings("ignore")
-
+from pandas import DataFrame
 from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import MinMaxScaler
 
 
-from acquire import get_titanic_data
+def handle_missing_values(df):
+    return df.assign(
+        embark_town=df.embark_town.fillna('Other'),
+        embarked=df.embarked.fillna('O'))
 
-df = get_titanic_data()
-df.embark_town.fillna('Other', inplace=True)
-df.drop(columns=['deck'], inplace=True)
+def remove_columns(df):    
+    return df.drop(columns=['deck'])
 
-encoder = LabelEncoder()
-df.embarked.fillna('Unknown', inplace=True)
-encoder.fit(df.embarked)
-df.embarked = encoder.transform(df.embarked)
-
-from sklearn.model_selection import train_test_split
-
-train, test = train_test_split(
-    df, test_size=.30, random_state=123, stratify=df.survived)
-
-
-scaler = MinMaxScaler()
-scaler.fit(train[['age', 'fare']])
-train[['age', 'fare']] = scaler.transform(train[['age', 'fare']])
-test[['age', 'fare']] = scaler.transform(test[['age', 'fare']])    
+def encode_embarked(df):
+    encoder = LabelEncoder()
+    encoder.fit(df.embarked)
+    return df.assign(embarked_encode = encoder.transform(df.embarked))
+   
+def prep_titanic_data(df):
+    df = df\
+        .pipe(handle_missing_values)\
+        .pipe(remove_columns)\
+        .pipe(encode_embarked)
+    return df
+             
