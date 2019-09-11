4c24a86ca7e53f0be0d2efa855c3ba31b8377a13
David Espinola <daver787@gmail.com>
2019-09-11 11:19:23 -0500

    Added on to json imports exercise

--- python_exercises/4.5_import_exercises.py
+++ python_exercises/4.5_import_exercises.py
@@ -12,14 +12,16 @@ combinations('abcd',2)
 
 #Exercise 3
 import json
+from collections import defaultdict
 with open("profiles.json", "r") as read_file:
     data = json.load(read_file)
-summary_dict={}    
-for dic in data:
-    if dic['isActive']=='True':
-        summary_dict['active_count'] +=1
+summary_dict=defaultdict(int) 
+for dict in data:
+    summary_dict['total_users']+=1#total number of users counter
+    if dict['isActive']=='true':
+        summary_dict['active_count']+=1#active users counter
     else:
-        summary_dict['inactive_count']+=1
+        summary_dict['inactive_count']+=1#inactive users counter
 
-
-
+    summary_dict['grand_total']=summary_dict['grand_total']+float(dict['balance'][1:].replace(",",""))#grand total accumulator
+    summary_dict["average_balance"]=summary_dict["grand_total"]/summary_dict["total_users"]#average balance per user
