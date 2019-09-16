a525793c3c7e21874cac556f7a594cc12f128edb
David Espinola <daver787@gmail.com>
2019-09-15 19:25:40 -0500

    Woeked on import exercises.

--- python_exercises/4.5_import_exercises.py
+++ python_exercises/4.5_import_exercises.py
@@ -12,16 +12,32 @@ combinations('abcd',2)
 
 #Exercise 3
 import json
-from collections import defaultdict
+import re
+from collections import defaultdict,Counter
 with open("profiles.json", "r") as read_file:
     data = json.load(read_file)
-summary_dict=defaultdict(int) 
+summary_dict=defaultdict(int)
+fruit_list=[]
+unread_messages=[]
 for dict in data:
     summary_dict['total_users']+=1#total number of users counter
     if dict['isActive']=='true':
         summary_dict['active_count']+=1#active users counter
     else:
         summary_dict['inactive_count']+=1#inactive users counter
-
     summary_dict['grand_total']=summary_dict['grand_total']+float(dict['balance'][1:].replace(",",""))#grand total accumulator
-    summary_dict["average_balance"]=summary_dict["grand_total"]/summary_dict["total_users"]#average balance per user
+    fruit_list.append(dict['favoriteFruit'])
+    unread_messages.append(dict['greeting'])
+summary_dict["average_balance"]=summary_dict["grand_total"]/summary_dict["total_users"]#average balance per user    
+counter=Counter(fruit_list)
+print(counter) 
+
+sum=0 
+for message in unread_messages:
+    r1=re.findall(r'\d+',message)
+    sum=sum+int(r1[0])  
+
+#example below is in the docs.
+d = defaultdict(list)
+for dict in data:
+    d[dict['balance']].append(dict['_id'])
